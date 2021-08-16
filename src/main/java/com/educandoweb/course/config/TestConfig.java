1ff5dedaf4a96fc3576cd5fea8c2f7382ff9dfa0
Vinicio Müller <vinicio.muller@outlook.com>
2021-08-16 00:56:37 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -86,7 +86,7 @@ public class TestConfig implements CommandLineRunner {
 		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		o1.setPayment(pay1);
 		
-		orderItemRepository.save(oi1);
+		orderRepository.save(o1);
 	}
 
 }
